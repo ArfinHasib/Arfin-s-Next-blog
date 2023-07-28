114d4b4b69135943582968793a9bcf0411eaeee6
ArfinHasib <arfinhasib21375@gmail.com>
2023-07-28 22:38:47 +0600

    finalVersion

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Hero from '../components/homepage/Hero';
+import Head from 'next/head';
+
 import FeaturedPosts from '../components/homepage/featured-posts';
 import { getFeaturedPosts } from '../lib/posts-util';
 
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Arfin's Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
